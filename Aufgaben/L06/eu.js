1c2009a0ba70b8e3ed3796593f65d2d3035e8d22
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-05-03 15:09:48 +0200

    Test

---
+++ Aufgaben/L06/eu.js
@@ -0,0 +1,6 @@
+var test = 1;
+var test2 = 15;
+var result = test + test2;
+alert(result);
+var animal = "Pingu";
+//# sourceMappingURL=eu.js.map
---
+++ Aufgaben/L06/eu.ts
@@ -0,0 +1,8 @@
+var test : number = 1;
+var test2 : number = 15;
+var result : number = test + test2;
+
+alert(result);
+
+var animal : string = "Pingu"
+
