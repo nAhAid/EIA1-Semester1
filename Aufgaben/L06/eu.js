ee16475764b0a0b6d9d08dd857e3236f253f0cf4
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-05-04 12:44:10 +0200

    Einwohnerzahl

--- Aufgaben/L06/eu.js
+++ Aufgaben/L06/eu.js
@@ -1,6 +1,12 @@
-var test = 1;
-var test2 = 15;
-var result = test + test2;
-alert(result);
-var animal = "Pingu";
+var eu_einwohner_gesamt = 447010000;
+var quelle_eu_einwohner_gesamt = "https://de.statista.com/themen/2332/bevoelkerung-in-eu-und-euro-zone/#dossierKeyfigures";
+var de_einwohner_gesamt = 83140000;
+var quelle_de_einwohner_gesamt = "https://de.statista.com/themen/2332/bevoelkerung-in-eu-und-euro-zone/#dossierKeyfigures";
+console.log(de_einwohner_gesamt / eu_einwohner_gesamt * 100 + " %");
+var fr_einwohner_gesamt = 65120000;
+var quelle_fr_einwohner_gesamt = "https://de.statista.com/statistik/daten/studie/19298/umfrage/gesamtbevoelkerung-in-frankreich/" + " Stand 2020";
+console.log(fr_einwohner_gesamt / eu_einwohner_gesamt * 100 + " %");
+var sp_einwohner_gesamt = 47110000;
+var quelle_sp_einwohner_gesamt = "https://de.statista.com/statistik/daten/studie/19315/umfrage/gesamtbevoelkerung-in-spanien/" + " Stand 2020";
+console.log(sp_einwohner_gesamt / eu_einwohner_gesamt * 100 + " %");
 //# sourceMappingURL=eu.js.map
--- Aufgaben/L06/eu.ts
+++ Aufgaben/L06/eu.ts
@@ -1,8 +1,21 @@
-var test : number = 1;
-var test2 : number = 15;
-var result : number = test + test2;
+var eu_einwohner_gesamt : number = 447010000;
+    var quelle_eu_einwohner_gesamt : string = "https://de.statista.com/themen/2332/bevoelkerung-in-eu-und-euro-zone/#dossierKeyfigures";
 
-alert(result);
 
-var animal : string = "Pingu"
+var de_einwohner_gesamt : number = 83140000;
+    var quelle_de_einwohner_gesamt : string = "https://de.statista.com/themen/2332/bevoelkerung-in-eu-und-euro-zone/#dossierKeyfigures";
+    console.log(de_einwohner_gesamt/eu_einwohner_gesamt*100 +" %");
+
+var fr_einwohner_gesamt : number =65120000;
+    var quelle_fr_einwohner_gesamt : string = "https://de.statista.com/statistik/daten/studie/19298/umfrage/gesamtbevoelkerung-in-frankreich/" +" Stand 2020";
+    console.log(fr_einwohner_gesamt/eu_einwohner_gesamt*100 +" %");
+
+var sp_einwohner_gesamt : number = 47110000;
+    var quelle_sp_einwohner_gesamt : string = "https://de.statista.com/statistik/daten/studie/19315/umfrage/gesamtbevoelkerung-in-spanien/" +" Stand 2020";
+    console.log(sp_einwohner_gesamt/eu_einwohner_gesamt*100 +" %");
+
+
+
+
+
 
