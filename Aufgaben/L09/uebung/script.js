097f1ac0fc21f412c26a44ab9a184694a2602faa
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-05-25 13:02:21 +0200

    Übung

---
+++ Aufgaben/L09/uebung/script.js
@@ -0,0 +1,15 @@
+var names = ["Ben", "Ella", "Emil", "Emilia", "Emma", "Finn", "Hannah", "Lea", "Leon", "Lina", "Louis", "Luca", "Marie", "Matteo", "Mia", "Mila", "Noah", "Paul", "Sophia", "Theo",];
+var names_zahl = names.length.toString();
+console.log(names.length);
+console.log(names_zahl);
+document.querySelector('#zahl').innerHTML = 'Gesamtzahl aller Namen: ' + names_zahl;
+function list() {
+    for (let index = 0; index < names.length; index++) {
+        console.log(names[index]);
+        document.querySelector('#liste').innerHTML += names[index] + "<br>";
+    }
+}
+document.querySelector('#buttonAufsteigend').addEventListener('click', function () {
+    list();
+});
+//# sourceMappingURL=script.js.map
---
+++ Aufgaben/L09/uebung/script.ts
@@ -0,0 +1,22 @@
+var names: string[] = ["Ben", "Ella", "Emil", "Emilia", "Emma", "Finn", "Hannah", "Lea", "Leon", "Lina", "Louis", "Luca", "Marie", "Matteo", "Mia", "Mila", "Noah", "Paul", "Sophia", "Theo",];
+
+var names_zahl: string = names.length.toString();
+console.log(names.length)
+console.log(names_zahl)
+document.querySelector('#zahl').innerHTML = 'Gesamtzahl aller Namen: ' + names_zahl;
+
+
+
+
+
+function list (){
+    for (let index = 0; index < names.length; index++) {
+        console.log(names[index])
+        document.querySelector('#liste').innerHTML += names[index] + "<br>";
+        
+    }
+}
+
+document.querySelector('#buttonAufsteigend').addEventListener('click', function () {
+    list()
+});
