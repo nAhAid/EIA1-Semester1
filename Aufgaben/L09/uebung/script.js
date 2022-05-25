0185d19dd30b9a98b505a57459f7f394f7b508ea
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-05-25 16:13:15 +0200

    warm-up

--- Aufgaben/L09/uebung/script.js
+++ Aufgaben/L09/uebung/script.js
@@ -4,11 +4,31 @@ console.log(names.length);
 console.log(names_zahl);
 document.querySelector('#zahl').innerHTML = 'Gesamtzahl aller Namen: ' + names_zahl;
 function list() {
+    var counter = 0;
+    if (counter = 0) {
+        list2();
+        counter = counter + 1;
+    }
+    else if (counter > 0) {
+    }
+    console.log(counter);
+}
+function list2() {
     for (let index = 0; index < names.length; index++) {
         console.log(names[index]);
         document.querySelector('#liste').innerHTML += names[index] + "<br>";
+        clearInterval(index);
     }
 }
+/**for (let index = 0; index < names.length; index++) {
+    console.log(names[index])
+    document.querySelector('#liste').innerHTML += names[index] + "<br>";
+
+    counter=counter+1;
+    console.log(counter);
+}
+
+}*/
 document.querySelector('#buttonAufsteigend').addEventListener('click', function () {
     list();
 });
--- Aufgaben/L09/uebung/script.ts
+++ Aufgaben/L09/uebung/script.ts
@@ -9,13 +9,39 @@ document.querySelector('#zahl').innerHTML = 'Gesamtzahl aller Namen: ' + names_z
 
 
 
-function list (){
+function list() {
+    var counter : number = 0;
+    if (counter = 0) {
+        list2();
+        counter = counter+1;
+      
+    }
+    else if (counter > 0) {
+
+    }
+    console.log(counter)
+}
+
+function list2(){
     for (let index = 0; index < names.length; index++) {
         console.log(names[index])
+
         document.querySelector('#liste').innerHTML += names[index] + "<br>";
-        
+
+        clearInterval(index)
     }
 }
+
+
+    /**for (let index = 0; index < names.length; index++) {
+        console.log(names[index])
+        document.querySelector('#liste').innerHTML += names[index] + "<br>";
+
+        counter=counter+1;
+        console.log(counter);
+    }
+
+}*/
 
 document.querySelector('#buttonAufsteigend').addEventListener('click', function () {
     list()
