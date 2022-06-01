a4ffe2c768602518774bc0b35544e6bcdf93ac8b
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-06-01 12:32:43 +0200

    finish_warm-up

--- Aufgaben/L10/warm-up/script.ts
+++ Aufgaben/L10/warm-up/script.ts
@@ -2,7 +2,6 @@ let numbers: number[] = [5, 17, 2, 34, 45, 45, 45, 6, 7576, 8, 86];
 let summe: number = 0;
 for (let index: number = 0; index < numbers.length; index++) {
     summe = summe + numbers[index];
-
 }
 console.log(summe);
 
