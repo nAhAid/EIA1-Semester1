790e1b79f373a1c726f0694e40465d5d5bda6c9c
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-06-06 17:27:55 +0200

    .

--- Aufgaben/L07/uebung_1/uebung.js
+++ Aufgaben/L07/uebung_1/uebung.js
@@ -2,12 +2,12 @@ function popup() {
     alert("der Daumen");
 }
 document.querySelector('#button1').addEventListener('click', popup);
-function counter() {
+function counter3() {
     document.querySelector('#result');
     zahl = zahl + 1;
     document.querySelector("#result").innerHTML = String(zahl);
 }
 var zahl = 0;
 var result = "1";
-document.querySelector('#button2').addEventListener('click', counter);
+document.querySelector('#button2').addEventListener('click', counter3);
 //# sourceMappingURL=uebung.js.map
--- Aufgaben/L07/uebung_1/uebung.ts
+++ Aufgaben/L07/uebung_1/uebung.ts
@@ -4,7 +4,7 @@ function popup(){
 
 document.querySelector('#button1').addEventListener('click', popup)
 
-function counter(){
+function counter3(){
     
     document.querySelector('#result');
     zahl=zahl+1;
@@ -15,5 +15,5 @@ function counter(){
 var zahl : number = 0;
 var result : string = "1";
 
-document.querySelector('#button2').addEventListener('click', counter);
+document.querySelector('#button2').addEventListener('click', counter3);
 
