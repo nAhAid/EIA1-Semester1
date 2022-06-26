3a4a358194c208e9b352790412b59b413fe1e565
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-06-26 11:36:17 +0200

    Sätze

--- Aufgaben/L11/script.ts
+++ Aufgaben/L11/script.ts
@@ -6,49 +6,88 @@ interface Satz {
 //Für jedes Objekt einzelnes Interface
 //Für jedes Array im Interface mit []-Klammern markieren
 let sammlung: Satz[] = [
-    {
-        deutsch: ["wort", "worte", "worte"],
-        spanisch: ["wort", "worte2", "worte"]
-
-    },
 
     {
-        deutsch: ["Wie", "heißt", "du"],
+        deutsch: ["Wie", "heißt", "du?"],
         spanisch: ["¿Cuál", "es", "su nombre?"]
     },
 
     {
-        deutsch: ["Ich", "liebe", "programmieren"],
-        spanisch: ["me", "encanta", "programar"]
+        deutsch: ["Ich", "liebe", "programmieren."],
+        spanisch: ["me", "encanta", "programar."]
 
     },
 
     {
-        deutsch: ["Ich", "mag", "Brezeln"],
-        spanisch: ["me", "gustan", "los pretzels"]
-    }, 
-
-    {
-        deutsch: ["Luke", "ich", "bin", "dein", "Vater"],
-        spanisch: ["Luke", "yo", "soy", "tu", "padre"]
+        deutsch: ["Ich", "mag", "Brezeln."],
+        spanisch: ["me", "gustan", "los pretzels."]
     },
 
     {
-        deutsch: [],
-        spanisch: []
+        deutsch: ["Luke", "ich", "bin", "dein", "Vater."],
+        spanisch: ["Luke", "yo", "soy", "tu", "padre."]
+    },
+
+    {
+        deutsch: ["Chantal,", "heul", "leise!"],
+        spanisch: ["¡Chantal,", "llora", "en silencio!"]
+    },
+
+    {
+        deutsch: ["Möge", "die", "Macht", "mit", "dir", "sein!"],
+        spanisch: ["¡Que", "la", "fuerza", "esté", "con", "usted!"]
+    },
+
+    {
+        deutsch: ["Nach", "Hause", "telefonieren!"],
+        spanisch: ["Para", "llamar", "a", "casa!"]
+    },
+
+    {
+        deutsch: ["Das", "Leben", "ist", "wie", "eine", "Schachtel", "Pralinen,", "man", "weiß", "nie", "was", "man", "kriegt."],
+        spanisch: ["La", "vida", "es", "como", "una", "caja", "de bombones,", "nunca", "sabes lo", "que te va", "a tocar"]
+    },
+
+    {
+        deutsch: ["Dumm", "ist", "der,", "der", "dummes", "tut."],
+        spanisch: ["Tonto", "es", "el", "que", "hace", "tonterías."]
+    },
+
+    {
+        deutsch: ["Yippie-Ya-Yeah,", "Schweinebacke"],
+        spanisch: ["¡Yippie-Ya-Sí,", "mejilla de cerdo!"]
+    },
+
+    {
+        deutsch: ["Mein", "Name", "ist", "Bond.", "James", "Bond."],
+        spanisch: ["Mi", "nombre", "es", "Bond.", "James", "Bond"]
+    },
+
+    {
+        deutsch: ["Ich", "bin", "der", "König", "der", "Welt!"],
+        spanisch: ["¡Soy", "el", "rey", "del", "mundo!"]
+    },
+
+    {
+        deutsch: ["Lauf", "Forrest!", "Lauf!"],
+        spanisch: ["¡Corre", "Forrest!", "¡correr!"]
+    },
+
+    {
+        deutsch: ["Für", "Frodo"],
+        spanisch: ["¡Por", "Frodo"]
+    },
+
+    {
+        deutsch: ["Der", "Winter", "naht!"],
+        spanisch: ["¡Viene", "el", "invierno!"]
     }
+
 ];
+
+console.log(sammlung.length);
 
 function randomize(arrayPoint: number): void {
     //let shuffled = Array.from(sammlung).sort(() => 0.5 - Math.random());
-
-};
-let shuffled = Array.from(sammlung).sort(() => 0.5 - Math.random());
-console.log(shuffled);
-
-
-for (let index: number = 0; index < sammlung.length; index++) {
-    console.log(index);
-
 
 }
